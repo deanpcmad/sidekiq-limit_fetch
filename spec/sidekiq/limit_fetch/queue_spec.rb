b168265a62b314e6e239b85f3f017bd899c570dd
brainopia <brainopia@evilmartians.com>
2013-01-17 19:46:17 +0400

    Update specs for queue

--- spec/sidekiq/limit_fetch/queue_spec.rb
+++ spec/sidekiq/limit_fetch/queue_spec.rb
@@ -23,5 +23,50 @@ describe Sidekiq::Queue do
       let(:constructor) { :[] }
       it_behaves_like :constructor
     end
+
+    context '#acquire' do
+      let(:queue) { Sidekiq::Queue['example'] }
+
+      it 'should be available' do
+        queue.acquire.should be
+      end
+
+      it 'should be pausable' do
+        queue.pause
+        queue.acquire.should_not be
+      end
+
+      it 'should be continuable' do
+        queue.pause
+        queue.continue
+        queue.acquire.should be
+      end
+
+      it 'should be limitable' do
+        queue.limit = 1
+        queue.acquire.should be
+        queue.acquire.should_not be
+      end
+
+      it 'should be resizable' do
+        queue.limit = 0
+        queue.acquire.should_not be
+        queue.limit = nil
+        queue.acquire.should be
+      end
+
+      it 'should be countable' do
+        queue.limit = 3
+        5.times { queue.acquire }
+        queue.busy.should == 3
+      end
+
+      it 'should be releasable' do
+        queue.acquire
+        queue.busy.should == 1
+        queue.release
+        queue.busy.should == 0
+      end
+    end
   end
 end
