deanpcmad/sidekiq-limit_fetch

commit 84ef650ea9bc5d7ec4bb288a594ae8b9bbeff34e

Fix #52, keep dynamic limits from initializers

lib/sidekiq/limit_fetch/queues.rb
@@ -46,7 +46,7 @@ class Sidekiq::LimitFetch
       limits ||= {}
       each_queue do |queue|
         limit = limits[queue.name.to_s] || limits[queue.name.to_sym]
-        queue.send "#{limit_type}=", limit
+        queue.send "#{limit_type}=", limit unless queue.limit_changed?
       end
     end
 

lib/sidekiq/extensions/queue.rb
@@ -4,10 +4,10 @@ module Sidekiq
     attr_reader :rname
 
     def_delegators :lock,
-      :limit,         :limit=,
+      :limit,         :limit=,         :limit_changed?,
       :process_limit, :process_limit=,
       :acquire,       :release,
-      :pause, :pause_for_ms, :unpause,
+      :pause,         :pause_for_ms,   :unpause,
       :block,         :unblock,
       :paused?,       :blocking?,
       :unblocked,     :block_except,

lib/sidekiq/limit_fetch/global/semaphore.rb
@@ -18,11 +18,17 @@ module Sidekiq::LimitFetch::Global
     end
 
     def limit=(value)
+      @limit_changed = true
+
       if value
         redis {|it| it.set "#{PREFIX}:limit:#@name", value }
       else
         redis {|it| it.del "#{PREFIX}:limit:#@name" }
       end
+    end
+
+    def limit_changed?
+      @limit_changed
     end
 
     def process_limit

spec/sidekiq/extensions/queue_spec.rb
@@ -84,6 +84,13 @@ describe Sidekiq::Queue do
         queue.unblock
         queue.should_not be_blocking
       end
+
+      it 'should be marked as changed' do
+        queue = Sidekiq::Queue["uniq_#{name}"]
+        queue.should_not be_limit_changed
+        queue.limit = 3
+        queue.should be_limit_changed
+      end
     end
   end
 end

commit 3ea94e6bdea81908de4b6b45033d4cdb1103cc8c

Set process_limits before limits
Swap the order because setting limits before process_limits makes
the queue.limit_changed? conditional in #set return true and so
process_limits don't get set.

It looks like this bug was introduced with commit
84ef650ea9bc5d7ec4bb288a594ae8b9bbeff34e

lib/sidekiq/limit_fetch/queues.rb
@@ -9,8 +9,8 @@ class Sidekiq::LimitFetch
 
       options[:strict] ? strict_order! : weighted_order!
 
-      set :limit, options[:limits]
       set :process_limit, options[:process_limits]
+      set :limit, options[:limits]
       set_blocks options[:blocking]
     end
 

spec/sidekiq/limit_fetch/queues_spec.rb
@@ -3,16 +3,18 @@ require 'spec_helper'
 RSpec.describe Sidekiq::LimitFetch::Queues do
   subject { described_class.new options }
 
-  let(:queues)   { %w[queue1 queue2] }
-  let(:limits)   {{ 'queue1' => 3 }}
-  let(:strict)   { true }
-  let(:blocking) {}
+  let(:queues)         { %w[queue1 queue2] }
+  let(:limits)         {{ 'queue1' => 3 }}
+  let(:strict)         { true }
+  let(:blocking)       {}
+  let(:process_limits) {{ 'queue2' => 3 }}
 
   let(:options) do
     { queues:   queues,
       limits:   limits,
       strict:   strict,
       blocking: blocking,
+      process_limits: process_limits,
       namespace: Sidekiq::LimitFetch::Redis.determine_namespace }
   end
 
@@ -80,6 +82,11 @@ RSpec.describe Sidekiq::LimitFetch::Queues do
     subject
     expect(Sidekiq::Queue['queue1'].limit).to eq 3
     expect(Sidekiq::Queue['queue2'].limit).not_to be
+  end
+
+  it 'should set process_limits' do
+    subject
+    expect(Sidekiq::Queue['queue2'].process_limit).to eq 3
   end
 
   context 'without strict flag' do