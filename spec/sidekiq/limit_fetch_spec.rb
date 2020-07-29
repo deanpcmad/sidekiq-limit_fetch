01f100399c005643a624a7a6b2ab9238352f5bb3
Vincent Ollivier <vincent.ollivier@augment.com>
2020-07-29 12:27:02 +0200

    Add test for bulk_requeue

--- spec/sidekiq/limit_fetch_spec.rb
+++ spec/sidekiq/limit_fetch_spec.rb
@@ -10,13 +10,16 @@ RSpec.describe Sidekiq::LimitFetch do
 
     Sidekiq.redis do |it|
       it.del 'queue:queue1'
-      it.lpush 'queue:queue1', 'task1'
-      it.lpush 'queue:queue1', 'task2'
+      it.del 'queue:queue2'
       it.expire 'queue:queue1', 30
     end
   end
 
   it 'should acquire lock on queue for execution' do
+    Sidekiq.redis do |it|
+      it.lpush 'queue:queue1', 'task1'
+      it.lpush 'queue:queue1', 'task2'
+    end
     work = subject.retrieve_work
     expect(work.queue_name).to eq 'queue1'
     expect(work.job).to eq 'task1'
@@ -44,5 +47,26 @@ RSpec.describe Sidekiq::LimitFetch do
 
     work = subject.retrieve_work
     expect(work.job).to eq 'task2'
+  end
+
+  it 'bulk requeues' do
+    Sidekiq.redis do |it|
+      it.lpush 'queue:queue1', 'task1'
+      it.lpush 'queue:queue2', 'task2'
+      it.lpush 'queue:queue2', 'task3'
+    end
+    q1 = Sidekiq::Queue['queue1']
+    q2 = Sidekiq::Queue['queue2']
+    expect(q1.size).to eq 1
+    expect(q2.size).to eq 2
+
+    fetch = subject.new(queues: ['queue1', 'queue2'])
+    works = 3.times.map { fetch.retrieve_work }
+    expect(q1.size).to eq 0
+    expect(q2.size).to eq 0
+
+    fetch.bulk_requeue(works, queues: [])
+    expect(q1.size).to eq 1
+    expect(q2.size).to eq 2
   end
 end
