2afeb8b2c290c305e17b78958a349a75a7a2d3ab
Bobby McDonald <bobbymcwho@gmail.com>
2022-08-16 13:14:31 -0400

    Updates that support Sidekiq 6.5+
    Some changes to Sidekiq internals require changes to how we pass
    options to certain Sidekiq classes we inherit from. These changes
    maintain backwards compatibility, but add some complexity to the
    code.

--- lib/sidekiq/limit_fetch/unit_of_work.rb
+++ lib/sidekiq/limit_fetch/unit_of_work.rb
@@ -1,7 +1,11 @@
 module Sidekiq
   class LimitFetch::UnitOfWork < BasicFetch::UnitOfWork
     def initialize(queue, job)
-      super
+      if post_6_5?
+        super(queue, job, Sidekiq)
+      else
+        super
+      end
       redis_retryable { Queue[queue_name].increase_busy }
     end
 
@@ -16,6 +20,10 @@ module Sidekiq
     end
 
     private
+
+    def post_6_5?
+      Gem::Version.new(Sidekiq::VERSION) >= Gem::Version.new('6.5.0')
+    end
 
     def redis_retryable(&block)
       Sidekiq::LimitFetch.redis_retryable(&block)
